2300bea6a554684a201119fbd23fbaa0c85021a2
Samuel Baker <43793861+samuelcbaker@users.noreply.github.com>
2025-01-13 16:44:09 +0000

    add prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Sarah, estou doido para ter ver entrando de noiva dia 01/02. <br></br> p.s. estarei te esperando la no altar ❤️</h1>
+  return (
+    <h1>
+      Sarah, estou doido para ter ver entrando de noiva dia 01/02. <br></br>{" "}
+      p.s. estarei te esperando la no altar ❤️
+    </h1>
+  );
 }
 
 export default Home;
