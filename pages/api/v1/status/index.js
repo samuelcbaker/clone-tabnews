1fa1df45c33515c590db195616886b2adf96cb6a
Samuel Baker <43793861+samuelcbaker@users.noreply.github.com>
2025-05-27 15:17:32 +0000

    feat: finish status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ message: "success" });
+  const updatedAt = new Date().toISOString();
+
+  const resultVersion = await database.query("SHOW server_version;");
+  const versionValue = resultVersion.rows[0].server_version;
+
+  const resultMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = parseInt(
+    resultMaxConnections.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const resultActiveConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const activeConnectionsValue = resultActiveConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versionValue,
+        max_connections: maxConnectionsValue,
+        opened_connections: activeConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
